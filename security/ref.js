da4baac5caec96daa69260ed78d88b39cc043ac6
Omar S <osantiago.lpz@gmail.com>
2023-07-21 12:33:54 -0600

    link

--- security/ref.js
+++ security/ref.js
@@ -12,15 +12,14 @@ function inicio() {
 function search() {
 	let url = document.getElementById('url').value;
 	https://www.whois.com/whois/google.com
-	document.body.innerHTML = "<p><a target='_blank' href='https://www.whois.com/whois/" + url + "'> Whois </a></p>"
-	document.body.innerHTML += "<p><a target='_blank' href='https://sitereview.bluecoat.com/#/lookup-result/" + url + "'> Symatec URL </a></p>"
-	document.body.innerHTML += "<p><a target='_blank' href='https://www.virustotal.com/gui/domain/" + url + "'> VirusTotal URL </a></p>"
-	document.body.innerHTML += "<p><a target='_blank' href='https://www.abuseipdb.com/check/" + url + "'> AbuseIPDB - IP </a></p>"
-	document.body.innerHTML += "<p><a target='_blank' href='https://dnschecker.org/?camp_opt=exact_match#A/" + url + "'> DNS Checker </a></p>"
-	document.body.innerHTML += "<p><a target='_blank' href='https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a" + url + "&run=toolpage'> MxToolbox </a></p>"
-
-
-
+	document.body.innerHTML = "<p><a target='_blank' href='https://www.whois.com/whois/" + url + "'> https://www.whois.com/whois/" + url + " </a></p>"
+	document.body.innerHTML += "<p><a target='_blank' href='https://sitereview.bluecoat.com/#/lookup-result/" + url + "'> https://sitereview.bluecoat.com/#/lookup-result/" + url + " </a></p>"
+	document.body.innerHTML += "<p><a target='_blank' href='https://www.virustotal.com/gui/domain/" + url + "'> https://www.virustotal.com/gui/domain/ " + url + " </a></p>"
+	document.body.innerHTML += "<p><a target='_blank' href='https://www.abuseipdb.com/check/" + url + "'> https://www.abuseipdb.com/check/" + url + " </a></p>"
+	document.body.innerHTML += "<p><a target='_blank' href='https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a" + url + "&run=toolpage'> https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a" + url + "&run=toolpage </a></p>"
+	document.body.innerHTML += "<p><a target='_blank' href='https://sitecheck.sucuri.net/?scan=" + url + "'> https://sitecheck.sucuri.net/?scan=" + url + " </a></p>"
+	document.body.innerHTML += "<p><a target='_blank' href='https://www.urlvoid.com/scan/" + url + "'> https://www.urlvoid.com/scan/" + url + " </a></p>"
+	document.body.innerHTML += "<p><a target='_blank' href='https://dnschecker.org/?camp_opt=exact_match#A/" + url + "'> https://dnschecker.org/?camp_opt=exact_match#A/" + url + " </a></p>"
 	
 }
 
