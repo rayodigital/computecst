26a5c493e3c4e74b5c58abad7781e91418d39986
rayodigital <96742532+rayodigital@users.noreply.github.com>
2023-08-22 08:35:22 -0500

    Update ref.js

--- security/ref.js
+++ security/ref.js
@@ -13,14 +13,13 @@ function search()
 {
 	let url = document.getElementById('url').value;
 	
+	
 	document.body.innerHTML += "<p><a target='_blank' href='https://dnschecker.org/?camp_opt=exact_match#A/" + url + "'> https://dnschecker.org/?camp_opt=exact_match#A/" + url + " </a></p>"
-	document.body.innerHTML += "<p><a target='_blank' href='https://dnschecker.org/?camp_opt=exact_match#A/" + url + "'> https://dnschecker.org/?camp_opt=exact_match#A/" + url + " </a></p>"
-	document.body.innerHTML = "<p><a target='_blank' href='https://www.whois.com/whois/" + url + "'> https://www.whois.com/whois/" + url + " </a></p>"
+	document.body.innerHTML += "<p><a target='_blank' href='https://www.whois.com/whois/" + url + "'> https://www.whois.com/whois/" + url + " </a></p>"
 	document.body.innerHTML += "<p><a target='_blank' href='https://sitereview.bluecoat.com/#/lookup-result/" + url + "'> https://sitereview.bluecoat.com/#/lookup-result/" + url + " </a></p>"
 	document.body.innerHTML += "<p><a target='_blank' href='https://www.virustotal.com/gui/domain/" + url + "'> https://www.virustotal.com/gui/domain/" + url + " </a></p>"
 	document.body.innerHTML += "<p><a target='_blank' href='https://www.abuseipdb.com/check/" + url + "'> https://www.abuseipdb.com/check/" + url + " </a></p>"
 	document.body.innerHTML += "<p><a target='_blank' href='https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a" + url + "&run=toolpage'> https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a" + url + "&run=toolpage </a></p>"
-	document.body.innerHTML += "<p><a target='_blank' href='https://sitecheck.sucuri.net/?scan=" + url + "'> https://sitecheck.sucuri.net/?scan=" + url + " </a></p>"
 	document.body.innerHTML += "<p><a target='_blank' href='https://www.urlvoid.com/scan/" + url + "'> https://www.urlvoid.com/scan/" + url + " </a></p>"
 	document.body.innerHTML += "<p><a target='_blank' href='https://otx.alienvault.com/indicator/domain/" + url + "'> https://otx.alienvault.com/indicator/domain/" + url + " </a></p>"
 	document.body.innerHTML += "<p><a target='_blank' href='https://otx.alienvault.com/indicator/ip/" + url + "'> https://otx.alienvault.com/indicator/ip/" + url + " </a></p>"
